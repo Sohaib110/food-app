82a3db36cf0da749f190d4bae22687d7e839b72e
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-06-03 23:49:38 +0500

    fourth

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-function Search() {
+function Search(props) {
+  console.log(props);
+  const { getDataFromSearchComponent } = props;
+  const [inputValue, setinputValue] = useState("");
+  const handleInputvalue = (event) => {
+    const { value } = event.target;
+
+    setinputValue(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getDataFromSearchComponent(inputValue);
+  };
   return (
-    <form className="Search">
-      <input name="search" id="search" placeholder="Search Recipe" />
+    <form onSubmit={handleSubmit} className="Search">
+      <input
+        name="search"
+        onChange={handleInputvalue}
+        value={inputValue}
+        id="search"
+        placeholder="Search Recipe"
+      />
       <button type="submit">Search</button>
     </form>
   );
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Search from "../../components/search";
 
-function Homepage() {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+function Homepage(getData) {
+  const dummydata = "dummy data from homepage";
+  console.log(getData, "getData from homepage");
+  const getDataFromSearchComponent = () => {
+    return (
+      <div>
+        <Search
+          getDataFromSearchComponent={getDataFromSearchComponent}
+          dummydatacopy={dummydata}
+        />
+      </div>
+    );
+  };
 }
 
 export default Homepage;
